8fbf1d9316a3cb75e4f7345422d6e0867a71f4bb
arachchi <nuran.11@cse.mrt.ac.lk>
2016-01-08 21:49:13 +0530

    creating registerToServer method

--- src/lk/ac/mrt/cse/Node.java
+++ src/lk/ac/mrt/cse/Node.java
@@ -14,17 +14,36 @@ class Node implements Serializable {
 
     public static void main(String args[]){
         String command;
-        System.out.println("Enter the port you need to start the server eg 1:- START 9876 ");
-        Scanner scanner = new Scanner(System.in);
+        Server server;
+        Client client;
+        boolean begin = true,registration;
+        while(begin) {
+            System.out.println("Enter the port you need to start the server eg 1:- START 9876 ");
+            Scanner scanner = new Scanner(System.in);
 
-        command = scanner.next();
-        if(command.equals("START")){
-        String port = scanner.next();
-        Server server = new Server(port);
-        Client client = new Client(port);
-            server.start();
-            client.start();
+            command = scanner.next();
+            if (command.equals("START")) {
+                String port = scanner.next();
+                registration = registerToServer();
+                if(registration){
+                    server = new Server(port);
+                    client = new Client(port);
+                    server.start();
+                    client.start();
+                    begin=false;
+                }
+            }
         }
+    }
+
+    public static boolean registerToServer(){
+        //registration details are included here.
+        //Connect to the bootstrap server and get the list of nodes
+        //send these list of nodes to the server instance
+        //if you cannot connect to the server using the user name and the password, ask a port and other details again
+        //then connect to the bootstrap server again
+
+        return true;
     }
 
 
