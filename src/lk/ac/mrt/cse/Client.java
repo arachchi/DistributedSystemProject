40aaf53140347ba8ed9cbd50ce5b4655fc8d3ac2
arachchi <nuran.11@cse.mrt.ac.lk>
2016-01-08 21:37:07 +0530

    init and search functionality in the client end

--- src/lk/ac/mrt/cse/Client.java
+++ src/lk/ac/mrt/cse/Client.java
@@ -13,13 +13,14 @@ import java.util.Scanner;
 public class Client extends Thread {
     int size=1024;
     int port=9878;
+    int hops=5;
 
     public Client(String port){
         this.port = Integer.parseInt(port);
     }
 
 
-    public void init(){
+    public void sendRequest(String packet){
         try {
             DatagramSocket clientSocket = new DatagramSocket();
 
@@ -28,16 +29,14 @@ public class Client extends Thread {
             byte[] sendData;
             byte[] receiveData = new byte[size];
 
-            String sentence;
-
-            sentence = "INIT " + IPAddress.getHostAddress() + " " + port;
-            sendData = sentence.getBytes();
+            sendData = packet.getBytes();
 
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
             clientSocket.send(sendPacket);
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             String modifiedSentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
             System.out.println("FROM SERVER:" + modifiedSentence);
             clientSocket.close();
         }catch (Exception e){
@@ -50,11 +49,33 @@ public class Client extends Thread {
         while(true){
             System.out.println("Enter your command. eg 1:- INTI eg 2:- SEARCH file_name");
             command = scanner.next();
-            if(command.equals("INIT"))
+            if(command.equals("INIT")){
                 init();
+            }
+
             else if(command.equals("SEARCH")){
                 fileName = scanner.next();
+                search(fileName);
             }
+        }
+    }
+
+    public void init(){
+        try {
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            String packet = "INIT " + IPAddress.getHostAddress() + " " + port;
+            sendRequest(packet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void search(String book){
+        try {
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            String packet = "SEARCH " +book+ " "+hops+" "+IPAddress.getHostAddress() + " " + port;
+            sendRequest(packet);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
