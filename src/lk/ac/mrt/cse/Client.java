0c3347df81d23de34cd83b8a6c074c168a5ca39e
dilhasha <dilhasha.uom@gmail.com>
2016-01-10 09:05:56 +0530

    Client search functionality is modified.

--- src/lk/ac/mrt/cse/Client.java
+++ src/lk/ac/mrt/cse/Client.java
@@ -1,11 +1,13 @@
 package lk.ac.mrt.cse;
 
+import java.lang.reflect.Array;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Set;
 
 /**
  * @author nuran
@@ -18,7 +20,7 @@ public class Client extends Thread {
     int hops=5;
 	
 	ArrayList<String> fileList;
-    Hashtable<String, String> neighbourFileList;
+    Hashtable<String, ArrayList<String>> neighbourFileList;
     ArrayList<Connection> connections;// Routing Table
 
     public Client(String port){
@@ -26,7 +28,7 @@ public class Client extends Thread {
 		
 		fileList = new ArrayList<String>();
         connections = new ArrayList<Connection>();
-        neighbourFileList = new Hashtable<String, String>();
+        neighbourFileList = new Hashtable<String, ArrayList<String>>();
     }
 
 
@@ -79,38 +81,49 @@ public class Client extends Thread {
             e.printStackTrace();
         }
     }
-    public void search(String book){
+    public void search(String keyword){
 	
 		hops--;//should be handled in server side
         String packet = "";
-        boolean hasBook = false;//this node contains the file
+        boolean hasBook = false;//Flags whether this node contains the file
+        ArrayList<String> books=new ArrayList<String>();;
 
         String searchResults = "";//search results
         int no_files = 0;//no of search results
         for (String file : fileList) {
-            if (file.contains(book)) {
+            //If the keyword is contained in the file name as a word or set of words
+            if (file.matches(".*\\b"+keyword+"\\b.*")) {
                 hasBook = true;
-                searchResults += file + " ";
+                searchResults += " " + file ;
                 no_files++;
             }
         }
+        if(!hasBook){
+            books= containsKeyWord(neighbourFileList,keyword);
+        }
 
-
-        if (hasBook) {//this node has the book
-
+        if (hasBook) {//this node has the keyword
             InetAddress IPAddress = null;
             try {
                 IPAddress = InetAddress.getByName("localhost");
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            packet = "SEARCHOK " + no_files + " " + IPAddress.getHostAddress() + " " + port + " " + hops + " " + searchResults;
+            packet = "SEARCHOK " + no_files + " " + IPAddress.getHostAddress() + " " + port + " " + hops + searchResults;
 
-        } else if (neighbourFileList.containsKey(book)) {//neighbour nodes have the book
-            
-            String IP = neighbourFileList.get(book);
-            packet = "SEARCH " + book + " " + hops + " " + IP + " " + port;
-            
+        }
+        else if (!books.isEmpty()) {//neighbour nodes have the keyword
+
+            for(String book: books){
+                ArrayList<String> IPAddresses = neighbourFileList.get(book);
+                if(!IPAddresses.isEmpty()){
+                    for(String IP:IPAddresses ){
+                        packet = "SEARCH " + book + " " + hops + " " + IP + " " + port;
+                    }
+
+                }
+
+            }
         } else { //otherwise
             if (hops > 1) {
                 //number of hops should be checked at the server side by reading search message request 
@@ -119,7 +132,7 @@ public class Client extends Thread {
                 //forward the message if not expired
                 for (Connection connection : connections) {
                     String IP = connection.getIp(); //port = connection.getPort();
-                    packet = "SEARCH " + book + " " + hops + " " + IP + " " + port;
+                    packet = "SEARCH " + keyword + " " + hops + " " + IP + " " + port;
                 }
                 
             }
@@ -131,11 +144,22 @@ public class Client extends Thread {
 		/*
         try {
             InetAddress IPAddress = InetAddress.getByName("localhost");
-            String packet = "SEARCH " +book+ " "+hops+" "+IPAddress.getHostAddress() + " " + port;
+            String packet = "SEARCH " +keyword+ " "+hops+" "+IPAddress.getHostAddress() + " " + port;
             sendRequest(packet);
         }catch (Exception e){
             e.printStackTrace();
         }
 		*/
+    }
+
+    public ArrayList<String> containsKeyWord(Hashtable<String, ArrayList<String>> neighbourFileList,String keyword){
+        ArrayList<String> books=new ArrayList<String>();
+        Set<String> keys = neighbourFileList.keySet();
+        for(String key :keys){
+            if(key.matches(".*\\b"+keyword+"\\b.*")){
+                books.add(key);
+            }
+        }
+        return  books;
     }
 }
