eb6553bfefcbeba8dd0c69137e16b589341a89ac
kulakshi <kulakshi.me@gmail.com>
2016-01-21 20:23:42 +0530

    resolved conflicts

--- src/main/java/lk/ac/mrt/cse/Node.java
+++ src/main/java/lk/ac/mrt/cse/Node.java
@@ -153,7 +153,7 @@ class Node extends Observable implements Serializable {
             // System.out.println("Enter Username to Register with BS");
             //String userName = inFromUser.readLine();
 
-            InetAddress IP = InetAddress.getLocalHost(); //Node.getIp();
+            InetAddress IP = Node.getIp();
             String ipAddress = IP.getHostAddress();
             nodeIp = ipAddress;
 
